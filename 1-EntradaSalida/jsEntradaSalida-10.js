95f6c3ce68e7e3bf782fe5247df56a7e2115febb
miguel <miguel.Alvarez@syloper.com>
2023-02-05 23:04:26 -0300

    archivo

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,29 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let importe; 
+	let descuento;
+	let nuevoImporte;
+
+	//tomamos un importe por id y lo convertimos en número con parseint
+	importe = parseInt(document.getElementById("txtIdImporte").value);
+	// a la variable importe le saco el 25% y lo guardo en la variable descuento
+	descuento= importe *25 / 100;
+	// resto el importe menos el porcentaje de descuento para guardar en nuevoImporte el resultado
+	nuevoImporte = importe - descuento;
+    // muestro 
+	document.getElementById("txtIdResultado").value = nuevoImporte 
+
 }
+//opcion terminal => nueva terminal
+
+//subida al repo
+//git status (ver los cambios que tengo para subir);
+//git add . (pasar los cambios a staged)
+//git status (ver los cambios que tengo para subir);
+//git commit -m "titulo del commit"
+//git push
+
+//bajada
+//git pull
+
